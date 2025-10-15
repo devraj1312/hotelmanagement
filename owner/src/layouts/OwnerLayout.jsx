bec94a2f3570920355aa38f9cee317b401a8e255
srishti <sharmasrishti2524@gmail.com>
2025-10-15 11:43:10 +0530

    owner responsive code

--- owner/src/layouts/OwnerLayout.jsx
+++ owner/src/layouts/OwnerLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const OwnerLayout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="owner-panel">
-      <Sidebar />
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <div className="main-section">
-        <Header />
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <div className="content-area">
           <Outlet />
         </div>
